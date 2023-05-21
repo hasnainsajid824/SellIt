595b32681157a9530e4fba2c9f90c4d58ae04a0f
Hussy24 <hasajid882@gmail.com>
2023-05-21 18:34:10 +0500

    Home Screen

--- app/src/main/java/com/example/sellit/login_activity.java
+++ app/src/main/java/com/example/sellit/login_activity.java
@@ -2,13 +2,38 @@ package com.example.sellit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_activity extends AppCompatActivity {
-
+    Button login;
+    Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        login = findViewById(R.id.loginbtn);
+        login.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(login_activity.this,drawer_activity.class);
+                        startActivity(i);
+                    }
+                }
+        );
+
+        register = findViewById(R.id.registerbtn);
+        register.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(login_activity.this,signup_activity.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 }
